4b63af747ab5f7f1649645e63452228653a2d3ae
Junaid Malik <jmalik981@gmail.com>
2020-04-15 03:47:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  []
+ []
 end
 
 def create_an_array
-  [ "ketchup", "mayo", "mustard", "siracha"]
+ x=  [ "ketchup", "mayo", "mustard", "siracha"]
 end
 
 def add_element_to_end_of_array(array, element)
